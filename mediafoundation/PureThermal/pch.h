57fb7c966d2d0fc0fa2b42a1027b322e96cc6994
Gregor Mittag <4136328+ghellwig@users.noreply.github.com>
2019-04-30 11:10:22 +0200

    Add Windows MediaFoundation sample.

---
+++ mediafoundation/PureThermal/pch.h
@@ -0,0 +1,32 @@
+﻿#ifndef PCH_H
+#define PCH_H
+
+#include <algorithm>
+#include <array>
+#include <atomic>
+#include <bitset>
+#include <condition_variable>
+#include <cstdint>
+#include <cstring>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <mutex>
+#include <sstream>
+#include <vector>
+
+#include <atlcomcli.h>
+#include <mfapi.h>
+#include <mferror.h>
+#include <mfplay.h>
+#include <mfreadwrite.h>
+#include <mmsystem.h>
+#include <ks.h>
+#include <ksproxy.h>
+#include <ksmedia.h>
+#include <vidcap.h>
+#include <shlwapi.h>
+
+#endif //PCH_H
