1ce74762fd04e577c43ec7e8d637427d3c0a37dd
kheirlb <kheirlbparks@gmail.com>
2019-05-30 11:34:54 -0700

    Update uvc-deviceinfo_ffc.py

--- ui_software/Record_IR/uvc-deviceinfo_ffc.py
+++ ui_software/Record_IR/uvc-deviceinfo_ffc.py
@@ -4,25 +4,25 @@
 from uvctypes import *
 
 def main():
-  ctx = POINTER(uvc_context)()
-  dev = POINTER(uvc_device)()
-  devh = POINTER(uvc_device_handle)()
-  ctrl = uvc_stream_ctrl()
+    ctx = POINTER(uvc_context)()
+    dev = POINTER(uvc_device)()
+    devh = POINTER(uvc_device_handle)()
+    ctrl = uvc_stream_ctrl()
 
-  res = libuvc.uvc_init(byref(ctx), 0)
-  if res < 0:
-    print("uvc_init error")
-    exit(1)
+    res = libuvc.uvc_init(byref(ctx), 0)
+    if res < 0:
+        print("uvc_init error")
+        exit(1)
     res = libuvc.uvc_find_device(ctx, byref(dev), PT_USB_VID, PT_USB_PID, 0)
     if res < 0:
-      print("uvc_find_device error")
-      exit(1)
-      res = libuvc.uvc_open(dev, byref(devh))
-      if res < 0:
+        print("uvc_find_device error")
+        exit(1)
+    res = libuvc.uvc_open(dev, byref(devh))
+    if res < 0:
         print("uvc_open error")
         exit(1)
 
-      print_device_info(devh)
+    print_device_info(devh)
 
 if __name__ == '__main__':
   main()
