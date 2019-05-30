edf4e69436a8b992fafc863b1d4573c9f0190f8d
kheirlb <kheirlbparks@gmail.com>
2019-05-30 15:33:53 -0700

    Update uvctypes_ffc.py

--- ui_software/Record_IR/uvctypes_ffc.py
+++ ui_software/Record_IR/uvctypes_ffc.py
@@ -253,12 +253,13 @@ def uvc_get_frame_formats_by_guid(devh, vs_fmt_guid):
   return []
 
 def set_manual_ffc(devh):
-    sizeData = 32 #32 bits?
+    sizeData = 2 #should be size 32 bits
     shutter_mode = create_string_buffer(sizeData)
     #0x200 Module ID VID
     #0x3C get
     #0x3D set
-    controlID = (0x3D >> 2) + 1
+    getSDK = 0x3D
+    controlID = (getSDK >> 2) + 1 #formula from Kurt Keifer
     print('controlID: ' + str(controlID))
-    call_extension_unit(devh, SYS_UNIT_ID, controlID, shutter_mode, sizeData) #set_extension_unit(devh, unit, control, data, size)
+    set_extension_unit(devh, SYS_UNIT_ID, controlID, shutter_mode, sizeData) #set_extension_unit(devh, unit, control, data, size)
     print("Shutter Mode #: {0}".format(repr(shutter_mode.raw)))
